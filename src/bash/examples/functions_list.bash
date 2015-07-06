1b06356080ad3bf0e44d5f4714a69fe2d717718c
Mark Veltzer <mark.veltzer@gmail.com>
2015-07-06 20:41:25 +0300

    notes about where to find more info

--- src/bash/examples/functions_list.bash
+++ src/bash/examples/functions_list.bash
@@ -4,6 +4,9 @@
 
 This example shows how to list all functions which have been declared.
 
+References:
+http://stackoverflow.com/questions/4471364/how-do-i-list-the-functions-defined-in-my-shell
+
 COMMENT
 
 function myfunc1 {
