605acc47933da3aaed3325a6235b3923bb80acc1
Mark Veltzer <mark.veltzer@gmail.com>
2015-11-23 09:53:22 +0200

    more docs

--- src/bash/examples/pid_and_ppid.bash
+++ src/bash/examples/pid_and_ppid.bash
@@ -5,6 +5,9 @@
 This example shows how to find your own pid and ppid
 from within a bash script.
 
+References:
+- http://www.linuxquestions.org/questions/programming-9/find-parent-pid-in-shell-script-526384/
+
 COMMENT
 
 echo "\$\$ is $$"
