52044c52eed6068a9b3ea06c0197396058c33756
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-08 11:11:58 +0300

    added two more examples

---
+++ src/bash/examples/booleans/booleans.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<<COMMENT
+
+This example shows how to use booleans in bash
+
+COMMENT
+
+val=false
+if $val; then
+	echo "val is true"
+else
+	echo "val is false"
+fi
---
+++ src/bash/examples/source/breakout_of_source.sh
@@ -0,0 +1,15 @@
+<<COMMENT
+
+When you 'source' files in bash you sometimes want to stop the 'source' itself.
+The trick is to use 'return' which cuts the 'source' short.
+
+COMMENT
+
+echo "before the if"
+
+if [ "$HOSTNAME" != nochance ]; then
+	echo "in the if"
+	return
+fi
+
+echo "after the if"
