47a1ab704d536e685bf32126ffb55ffdac296f98
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-09 18:38:18 +0300

    improved strict demos

--- src/bash/examples/strictness/halt_on_errors.bash
+++ src/bash/examples/strictness/halt_on_errors.bash
@@ -7,10 +7,16 @@ If you remove the "set -e" and "set +e" then the script
 will continue even though the "false" command fails.
 
 More thing to make bash more strict:
-set -u / set +u
-set -o pipefail
-or in short:
-set -euo pipefail
+
+give errors when commands fail:
+	set -e / set +e
+give errors when the shell is required to expand undefined variabled:
+	set -u / set +u
+give errors when pipes fail:
+	set -o pipefail:
+
+To sum it up, you can begin your script with:
+	set -euo pipefail
 
 References:
 http://redsymbol.net/articles/unofficial-bash-strict-mode/
---
+++ src/bash/examples/strictness/halt_on_unknown_varialbes.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# this script does not have -u on purpose
+
+<<COMMENT
+
+This shows how to make bash more strict in that it will error
+and will stop on any undefined variable that requires expansion.
+This shows how to make bash more strict
+If you remove the "set -e" and "set +e" then the script
+will continue even though the "false" command fails.
+
+More thing to make bash more strict:
+
+give errors when commands fail:
+	set -e / set +e
+give errors when the shell is required to expand undefined variabled:
+	set -u / set +u
+give errors when pipes fail:
+	set -o pipefail:
+
+To sum it up, you can begin your script with:
+	set -euo pipefail
+
+References:
+http://redsymbol.net/articles/unofficial-bash-strict-mode/
+
+COMMENT
+
+echo "before $UNDEFINED"
+set -u
+echo "in $UNDEFINED"
+set +u
+echo "after $UNDEFINED"
