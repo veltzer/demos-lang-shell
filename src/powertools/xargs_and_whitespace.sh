c0dd992b8157fc122a98461e8f47e674d079235f
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-16 16:36:57 +0300

    fixed up this example

--- src/powertools/xargs_and_whitespace.sh
+++ src/powertools/xargs_and_whitespace.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-# This example shows that xargs has problems processing file names
-# that have whitespace in them. The -n 1 is to make xargs activate
-# the command on each individual input and not combine them into
-# one activations.
-# as you can the number of activations of echo is problematic
-# the difference between number of words of wc of the entire
-# file and the number of echo activations is blank lines (there
-# are 2 of these in this script)
+<<'COMMENT'
+
+This example shows that xargs has problems processing file names
+hat have whitespace in them. The -n 1 is to make xargs activate
+the command on each individual input and not combine them into
+one activations.
+as you can the number of activations of echo is problematic
+the difference between number of words of wc of the entire
+file and the number of echo activations is blank lines (there
+are 2 of these in this script)
+
+COMMENT
 
 file=$0
 wc $file
