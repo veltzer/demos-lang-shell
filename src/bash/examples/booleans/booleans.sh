a7a996894b2fce29bc2ae36f398c0361551c4ca6
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-16 16:56:37 +0300

    added more boolean examples

--- src/bash/examples/booleans/booleans.sh
+++ src/bash/examples/booleans/booleans.sh
@@ -12,3 +12,27 @@ if $val; then
 else
 	echo "val is false"
 fi
+val=true
+if $val; then
+	echo "val is true"
+else
+	echo "val is false"
+fi
+val=false
+if ( ! $val ); then
+	echo "val is false"
+else
+	echo "val is true"
+fi
+a=true
+b=false
+if [ $a = true -a $b = true ]; then
+	echo "a -a b is true"
+else
+	echo "a -a b is false"
+fi
+if [ $a = true -o $b = true ]; then
+	echo "a -o b is true"
+else
+	echo "a -o b is false"
+fi
