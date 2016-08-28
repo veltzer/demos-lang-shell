c54f06f0970827c84234c72c83ea1deb8da6dd4c
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-28 16:07:57 +0300

    added example of xargs

---
+++ src/powertools/xargs/newlines.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+<<'COMMENT'
+
+This example shows how to combine xargs and output from commands
+which give one output per line.
+
+Note that the -d "\n" feature is a GNU only feature.
+
+COMMENT
+
+cat $0 | xargs -n 1 -d "\n" echo
+echo "==================="
+cat $0 | xargs -d "\n" echo
+echo "==================="
+cat $0 | xargs -n 1 echo
+echo "==================="
+cat $0 | xargs echo
--- src/powertools/xargs/whitespace.sh
+++ src/powertools/xargs/whitespace.sh
@@ -5,7 +5,7 @@
 This example shows that xargs has problems processing file names
 hat have whitespace in them. The -n 1 is to make xargs activate
 the command on each individual input and not combine them into
-one activations.
+one activation.
 as you can the number of activations of echo is problematic
 the difference between number of words of wc of the entire
 file and the number of echo activations is blank lines (there
