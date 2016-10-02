901fa02b0ba7cb13a480d5f4675122716e1c1e00
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-02 12:25:04 +0300

    fixed up this example

--- src/bash/examples_standalone/source_many_files/main.bash
+++ src/bash/examples_standalone/source_many_files/main.bash
@@ -4,6 +4,19 @@
 
 This example shows that you cannot source more than one file in bash.
 
+The loop which follows is the right way to do it.
+
+Another thing that works is the cat example.
+
+References:
+http://stackoverflow.com/questions/1423352/source-all-files-in-a-directory-from-bash-profile
+
 COMMENT
 
 source one.bash two.bash
+
+for x in ???.bash; do
+	source $x
+done
+
+source <(cat ???.bash)
