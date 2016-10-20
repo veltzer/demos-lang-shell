298dbb008dc7defbb32ebc6399040f4829716e12
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-20 18:04:50 +0300

    added a slice example

--- src/bash/examples/arrays/slice.bash
+++ src/bash/examples/arrays/slice.bash
@@ -3,14 +3,19 @@
 <<'COMMENT'
 
 This is an example of how to slice an array in bash.
+Run this example with command line arguments like this:
+$ ./slice.bash 1 2 3 4 5 6
 
 References:
 http://stackoverflow.com/questions/1335815/how-to-slice-an-array-in-bash
 
 COMMENT
 
-A=( "a" "b" "c" "d" "e" )
-B=("${A[@]:1}")
+echo "${@:3:2}"
+A=( "a" "b" "c" "d" "e" "f" "g")
+B=("${A[@]:3}")
+C=("${A[@]:3:2}")
 
 echo "${A[@]}"
 echo "${B[@]}"
+echo "${C[@]}"
