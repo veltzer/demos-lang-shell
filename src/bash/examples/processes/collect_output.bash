71040fea1f252c51c69d4ad6fe9b51e7db1a8db1
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-24 17:04:35 +0300

    added references

--- src/bash/examples/processes/collect_output.bash
+++ src/bash/examples/processes/collect_output.bash
@@ -6,6 +6,9 @@ This example shows how to collect output of processes in bash.
 
 Basically there are two ways: backticks and $().
 
+References:
+- https://www.cyberciti.biz/faq/unix-linux-bsd-appleosx-bash-assign-variable-command-output/
+
 COMMENT
 
 x=$(date)
