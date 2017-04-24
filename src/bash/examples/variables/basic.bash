b701120e187280576bb877187fc5b5f4c1b2e094
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-24 13:25:59 +0300

    better example

--- src/bash/examples/variables/basic.bash
+++ src/bash/examples/variables/basic.bash
@@ -10,7 +10,10 @@ somevar=7
 echo $somevar
 # the next line will create an error since only "this" will be inserted
 # into the 'othervar' variable and the command 'is a var' will be attemted (and failed).
-#othervar=this is a var
+othervar=this is a var 2> /dev/null
+if [ $? != 0 ]; then
+	echo "yes, there was an error"
+fi
 # this is the correct way...
 othervar="this is a var"
 echo $othervar
