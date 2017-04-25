dd3ad729e73f9f8b81322076bcf0b772c6aa76fd
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-25 18:15:25 +0300

    more examples

---
+++ src/bash/examples/io/create_temp_file.bash
@@ -0,0 +1,17 @@
+#!/bin/bash -u
+
+<<'COMMENT'
+
+This example shows how to create a temporary file in bash(1).
+
+References:
+- https://www.cyberciti.biz/tips/shell-scripting-bash-how-to-create-temporary-random-file-name.html
+
+COMMENT
+
+tfile=$(mktemp /tmp/foo.XXXXXXXXX)
+if [ -f "$tfile" ]; then
+	echo "yes, the file [$tfile] is there"
+fi
+
+rm $tfile
