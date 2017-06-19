9239b081f3b6006f88f651906e3a12283feaf8f7
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-19 13:20:46 +0300

    example of array of functions

---
+++ src/bash/examples/functions/array_of_functions.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -u
+
+<<'COMMENT'
+
+This example shows how to store an array of functions.
+
+References:
+- https://stackoverflow.com/questions/20361398/bash-array-of-functions
+
+COMMENT
+
+function a {
+	echo "a"
+}
+
+function b {
+	echo "b"
+}
+
+declare -a array
+array+=(a)
+array+=(b)
+
+# run all functions
+for i in "${!array[@]}"; do
+	echo "running [${array[$i]}]"
+	${array[$i]}
+done
