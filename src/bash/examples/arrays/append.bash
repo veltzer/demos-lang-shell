c8e301e7e4a66167f0a9c380df763df414bf7c0e
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-19 11:59:03 +0300

    reference

--- src/bash/examples/arrays/append.bash
+++ src/bash/examples/arrays/append.bash
@@ -5,6 +5,7 @@
 This is an example of how to append to an array in bash.
 
 References:
+- https://stackoverflow.com/questions/1951506/add-a-new-element-to-an-array-without-specifying-the-index-in-bash
 
 COMMENT
 
