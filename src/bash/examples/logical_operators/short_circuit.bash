9a89ca484e532db8b251c5fabd59c16f7fd0c5e9
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-20 14:18:15 +0300

    another good example

---
+++ src/bash/examples/logical_operators/short_circuit.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -u
+
+<<'COMMENT'
+
+This example explores the short circuit feature of bash boolean operators.
+
+Notes:
+- bash does have a short circuit feature.
+- sometimes short circuiting is what you want.
+- if this is not the case then make sure to execute both parts
+of the logical condition and only then evaluate the results.
+
+COMMENT
+
+true || echo "where is this?"
+false || echo "you will see this"
+false && echo "where is this?"
+true && echo "you will see this"
