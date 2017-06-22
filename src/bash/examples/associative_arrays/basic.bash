f5b695428aca4997afd87e16aa6dbe146f0ebed3
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-23 00:38:14 +0300

    associative array in bash example

---
+++ src/bash/examples/associative_arrays/basic.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -u
+
+<<'COMMENT'
+
+This example shows how to use associative array in bash.
+
+NOTES:
+- this is a bash 4 feature. will not work on older versions.
+
+References:
+- https://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
+
+COMMENT
+
+declare -A hashmap
+hashmap["key"]="value"
+hashmap["key2"]="value2"
+echo "${hashmap["key"]}"
+for key in ${!hashmap[@]}; do echo $key; done
+for value in ${hashmap[@]}; do echo $value; done
+echo hashmap has ${#hashmap[@]} elements
