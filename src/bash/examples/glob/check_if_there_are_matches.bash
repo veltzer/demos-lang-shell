62c2e2884f1098794887b068d417733305ef5d23
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-25 14:25:38 +0300

    more on globbing

--- src/bash/examples/glob/check_if_there_are_matches.bash
+++ src/bash/examples/glob/check_if_there_are_matches.bash
@@ -3,6 +3,8 @@
 <<'COMMENT'
 
 This example shows how to check if a glob patten has matches.
+This uses the compgen shell builtin
+documentation on compgen is at 'man builtins'
 
 References:
 - http://stackoverflow.com/questions/2937407/test-whether-a-glob-has-any-matches-in-bash
---
+++ src/bash/examples/glob/iterate_on_glob.bash
@@ -0,0 +1,36 @@
+#!/bin/bash -u
+
+<<'COMMENT'
+
+This example shows how to iterate on a bunch of files.
+The easiest way is to use straight up bash but to use
+the 'shopt -s nullglob'.
+
+
+COMMENT
+
+source ../../includes/common.bash
+
+
+
+shopt -s nullglob
+
+echo -e "trying using expanding globs into arrays"
+echo -e "\tthis list should be empty"
+a=(*.notexists)
+for x in "${a[@]}"; do echo -e "\t\t$x"; done
+echo -e "\tthis list should be full"
+a=(*.bash)
+for x in "${a[@]}"; do echo -e "\t\t$x"; done
+
+echo -e "trying using straight up bash"
+echo -e "\tthis list should be empty"
+for x in *.notexists; do echo -e "\t\t$x"; done
+echo -e "\tthis list should be full"
+for x in *.bash; do echo -e "\t\t$x"; done
+
+echo -e "trying using compgen"
+echo -e "\tthis list shold be empty"
+for x in $(compgen -G "*.notexists"); do echo -e "\t\t$x"; done
+echo -e "\tthis list should be full"
+for x in $(compgen -G "*.bash"); do echo -e "\t\t$x"; done
