21e5862822a87de06bede3b9967180d6e40a57b4
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-25 11:58:29 +0300

    more docs about integer arithmentic

--- src/bash/examples/arithmetic/basic.bash
+++ src/bash/examples/arithmetic/basic.bash
@@ -2,7 +2,11 @@
 
 <<'COMMENT'
 
-This is an example of basic arithmetic in the shell.
+This is an example of basic integer arithmetic in the shell using the 'let' shell builtin.
+
+NOTES:
+- You can use the 'let' shell builtin in functions too.
+- You can find documentation about the 'let' shell builtin in 'man builtins'.
 
 COMMENT
 
@@ -15,3 +19,12 @@ echo $x
 # yes, the bash has ++
 let "x++"
 echo $x
+
+function foo() {
+	let "x=0"
+	echo $x
+	let "x=x+1"
+	echo $x
+}
+
+foo
