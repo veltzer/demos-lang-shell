9ef8fbbf2828a78869ff98b533fee63fa1683858
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-25 17:16:11 +0300

    notes on returning a boolean value from a bash function

--- src/bash/examples/functions/return_a_boolean_value.bash
+++ src/bash/examples/functions/return_a_boolean_value.bash
@@ -4,6 +4,13 @@
 
 This is how to return a boolean value from a bash function.
 
+Notes:
+- you cannot write code like this:
+	return [ ... ]
+	the right way to write this code would be:
+	[ ... ]
+	return $?
+
 References:
 - https://stackoverflow.com/questions/5431909/bash-functions-return-boolean-to-be-used-in-if
 
@@ -17,5 +24,11 @@ function no {
 	return 1
 }
 
+function func() {
+	[ -f "/etc/passwd" ]
+	return $?
+}
+
 if yes; then echo "yes"; fi
 if ! no ; then echo "no"; fi
+if func; then echo "yes, /etc/passwd is readable"; fi
