e2decd73a63186638375552c24bfcf55bf81fecf
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-27 11:33:07 +0300

    more bash notes

--- src/bash/examples/functions/return_a_boolean_value.bash
+++ src/bash/examples/functions/return_a_boolean_value.bash
@@ -10,6 +10,7 @@ Notes:
 	the right way to write this code would be:
 	[ ... ]
 	return $?
+- you can use implicit return. $? is your return value if you do not return a value.
 
 References:
 - https://stackoverflow.com/questions/5431909/bash-functions-return-boolean-to-be-used-in-if
@@ -25,10 +26,20 @@ function no {
 }
 
 function func() {
-	[ -f "/etc/passwd" ]
+	[ -r "/etc/passwd" ]
 	return $?
+}
+
+function implicit_return_1() {
+	[ -r "/etc/passwd" ]
+}
+
+function implicit_return_2() {
+	[ -r "/etc/shadow" ]
 }
 
 if yes; then echo "yes"; fi
 if ! no ; then echo "no"; fi
 if func; then echo "yes, /etc/passwd is readable"; fi
+if implicit_return_1; then echo "yes, /etc/passwd is readable"; fi
+if ! implicit_return_2; then echo "yes, /etc/shadow is unreadable"; fi
