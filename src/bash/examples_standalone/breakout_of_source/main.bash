e1e7512610bece92bd0847d05f23d5fd140fc31a
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-04 11:56:14 +0300

    better example

--- src/bash/examples_standalone/breakout_of_source/main.bash
+++ src/bash/examples_standalone/breakout_of_source/main.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "in main"
 source sourced_file.sh
+echo "back in main $?"
--- src/bash/examples_standalone/breakout_of_source/sourced_file.sh
+++ src/bash/examples_standalone/breakout_of_source/sourced_file.sh
@@ -2,6 +2,8 @@
 
 When you 'source' files in bash you sometimes want to stop the 'source' itself.
 The trick is to use 'return' which cuts the 'source' short.
+You can even use the value returned using the 'return' command. If you don't put
+a value then you return 0 by default. You collect the returned value using '$?'.
 
 COMMENT
 
