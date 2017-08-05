8efd10839d6d1033180f2c4a1d9390d41e191399
Mark Veltzer <mark.veltzer@gmail.com>
2017-08-05 09:57:33 +0300

    elaborate the example

--- src/bash/examples_standalone/source_many_files/main.bash
+++ src/bash/examples_standalone/source_many_files/main.bash
@@ -7,7 +7,11 @@ The loop which follows is the right way to do it.
 Another thing that does work is the cat example.
 
 Another issue is what happens if there are no files matching the pattern.
-In this case just doing the for loop is not enough.
+In this case just doing the for loop is not enough so we have to do
+one of the following:
+- only apply the glob after we checked that there are files matching the glob
+as in the example of the 'compgen -G' below.
+- use the shell option 'shopt -s nullglob' and 'shopt -u nullglob'
 
 References:
 http://stackoverflow.com/questions/1423352/source-all-files-in-a-directory-from-bash-profile
@@ -19,12 +23,26 @@ COMMENT
 # -will fail if there is no file matching the pattern
 source subfolder/*.bash
 
+# this one checks if there are matches before doing
+# regular shell globbing
+# so this handles the case where there are no matches.
+# it is long though
 if compgen -G "subfolder/*.bash" > /dev/null; then
 	for x in subfolder/*.bash; do
 		source $x
 	done
 fi
-	
+
+# the following example does handle the case where there are no matches
+# the difficulty is that we need to return the status of the shell 'nullglob'
+# setting to it's original state and we do not do that here.
+shopt -s nullglob
+for x in subfolder/*.bash; do
+	source $x
+done
+shopt -u nullglob
+
+# this handles no matches and is short
 for x in $(compgen -G "subfolder/*.bash"); do
 	source $x
 done
